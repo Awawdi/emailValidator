e75af1d97274a738fe546d749eeb8f48a139214b
Awawdi <orsan.awawdi@gmail.com>
2019-04-15 13:33:34 +0300

    Add files via upload
    PS C:\Users\orsana\Desktop\start> dotnet run
    Please enter your email address to verify
    test@test.com.1
    checking email address : test@test.com.1
    email address is invalid
    PS C:\Users\orsana\Desktop\start> dotnet run
    Please enter your email address to verify
    orsan.awawdi@gmail.com
    checking email address : orsan.awawdi@gmail.com
    Email Address is correct!
    PS C:\Users\orsana\Desktop\start>

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Implementation emailChecker = new Implementation();
+            Console.WriteLine("Please enter your email address to verify");
+            string emailAddress = Console.ReadLine();
+            Console.WriteLine("checking email address : "+ emailAddress);
+            Console.WriteLine(emailChecker.callValidator(emailAddress));
+            
+        }
+
+        public class Implementation
+        {
+        public String callValidator(string userEmail)
+        {
+        Regex emailRegex = new Regex(@"^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$");
+        bool valid = true;        
+        String Message="";
+
+        if (userEmail.Length == 0)
+        {
+            Message += "you must enter an email address";
+            valid = false;
+        }
+
+        if (!emailRegex.IsMatch(userEmail))
+        {
+            Message = "email address is invalid";
+            valid = false;
+        }
+
+        if (!valid)
+            return Message;
+            else
+            return "Email Address is correct!";
+        }
+        }
+    }
+}
